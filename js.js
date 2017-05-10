f08a5013f2434b5a0efda2313edb5e1ed78c9d63
Alexanderzu <artminor@yandex.ru>
2017-05-10 19:36:07 +0300

    ver 1

---
+++ js.js
@@ -0,0 +1,40 @@
+// (function() {
+
+// 	var doc = document.documentElement;
+
+// 	var accordeonItem = doc.querySelectorAll('.accordeon__item'),
+// 		accordeonItemTrigger = doc.querySelectorAll('.accordeon__item_trigger'),
+// 		accordeonItemContent = doc.querySelectorAll('.accordeon__item_content');
+
+
+// 	for (var i = 0; i < accordeonItemTrigger.length; i++) {
+// 	    // console.log(accordeonItemTrigger[i]);
+// 		accordeonItemTrigger[i].onclick = function(e) {
+// 			var parElem = this.parentNode;
+// 	    	parElem.classList.toggle("open");
+// 	    	// console.log("test");
+// 	    };
+// 	};
+
+// })();
+
+
+(function() {
+
+	var doc = document.documentElement,
+		accordeonItem = doc.querySelectorAll('.accordeon__item'),
+		accordeonItemTrigger = doc.querySelectorAll('.accordeon__item_trigger'),
+		accordeonItemContent = doc.querySelectorAll('.accordeon__item_content');
+
+	for (var i = 0; i < accordeonItemTrigger.length; i++) {
+	    // console.log(accordeonItemTrigger[i]);
+		accordeonItemTrigger[i].onclick = function(e) {
+			var parElem = this.parentNode;
+	    	parElem.classList.toggle("open");
+	    	// console.log("test");
+	    
+	    };
+	};
+
+})();
+
